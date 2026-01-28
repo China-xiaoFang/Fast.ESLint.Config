eee207810340ea54c3fb208bcabd41272db4f1cb
小方 <2875616188@qq.com>
2026-01-28 19:11:59 +0800

    `v1.0.42` 修复报错问题。

--- src/configs/import.ts
+++ src/configs/import.ts
@@ -15,7 +15,12 @@ export const importConfigs = defineConfig([
 			// 确保 ESLint 和 eslint-plugin-import 能够正确解析项目中的所有相关文件类型
 			"import/resolver": {
 				node: {
+					alwaysTryTypes: true,
 					extensions: [CONST_JS, CONST_JSX, CONST_TS, CONST_TSX, CONST_DTS],
+				},
+				typescript: {
+					alwaysTryTypes: true,
+					project: ["./tsconfig.json", "./tsconfig.*.json", "./packages/*/tsconfig.json", "./packages/*/tsconfig.*.json"],
 				},
 			},
 		},
