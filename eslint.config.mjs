7f2a2b40b6d0a862b5ac478fdb3262c8ceb41f36
小方 <2875616188@qq.com>
2026-08-03 00:12:53 +0800

    1

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -17,16 +17,18 @@ import {
 } from "@fast-china/eslint-config/configs";
 import { GLOBS_JAVASCRIPT, GLOBS_TYPESCRIPT } from "@fast-china/eslint-config/constants";
 
-const codeFiles = [...GLOBS_JAVASCRIPT, ...GLOBS_TYPESCRIPT];
-
 export default defineConfig([
 	...createGlobalIgnores(),
 	...createGitignoreConfigs(),
-	...createEnvironmentConfigs({ environment: "node", files: codeFiles, nodeFiles: codeFiles }),
-	...createCommonConfigs(codeFiles),
+	...createEnvironmentConfigs({
+		environment: "node",
+		files: [...GLOBS_JAVASCRIPT, ...GLOBS_TYPESCRIPT],
+		nodeFiles: [...GLOBS_JAVASCRIPT, ...GLOBS_TYPESCRIPT],
+	}),
+	...createCommonConfigs([...GLOBS_JAVASCRIPT, ...GLOBS_TYPESCRIPT]),
 	...createJavaScriptConfigs(),
-	...createImportConfigs(codeFiles),
-	...createRegexpConfigs(codeFiles),
+	...createImportConfigs([...GLOBS_JAVASCRIPT, ...GLOBS_TYPESCRIPT]),
+	...createRegexpConfigs([...GLOBS_JAVASCRIPT, ...GLOBS_TYPESCRIPT]),
 	...createTypeScriptConfigs({ typeChecked: true }),
 	...createJsonConfigs(),
 	...createPackageJsonSortConfigs(),
